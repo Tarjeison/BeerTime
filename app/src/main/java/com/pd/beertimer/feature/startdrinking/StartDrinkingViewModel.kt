1c611d53f64570dd0c25b0ccfe9441d5556970dc
Trym <Trym>
2020-09-29 09:57:20 +0200

    Remove loooong name drink

--- app/src/main/java/com/pd/beertimer/feature/startdrinking/StartDrinkingViewModel.kt
+++ app/src/main/java/com/pd/beertimer/feature/startdrinking/StartDrinkingViewModel.kt
@@ -7,9 +7,8 @@ import com.pd.beertimer.models.AlcoholUnit
 class StartDrinkingViewModel : ViewModel() {
 
     private val alcoholUnits = arrayListOf<AlcoholUnit>(
-        AlcoholUnit("Beer 0,33L", 0.333F,0.047F, R.drawable.ic_icon_beer),
-        AlcoholUnit("A really looong name", 0.500F,0.047F, R.drawable.ic_icon_beer),
-        AlcoholUnit("Beer 0,5L", 0.500F,0.047F, R.drawable.ic_icon_beer),
+        AlcoholUnit("Small beer", 0.33F,0.047F, R.drawable.ic_icon_beer),
+        AlcoholUnit("Large beer", 0.50F,0.047F, R.drawable.ic_icon_beer),
         AlcoholUnit("Wine", 0.150F,0.125F, R.drawable.ic_wine)
     )
 
